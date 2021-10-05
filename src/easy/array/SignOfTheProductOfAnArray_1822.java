e989fa64020ce05240636a5fc7155ee53678afa8
Yevgeniy Batulin <yevgeniy.batulin@gmail.com>
2021-10-05 22:53:15 +0300

    Easy arrays

---
+++ src/easy/array/SignOfTheProductOfAnArray_1822.java
@@ -0,0 +1,14 @@
+package easy.array;
+
+public class SignOfTheProductOfAnArray_1822 {
+    public int arraySign(int[] nums) {
+        int result = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                return 0;
+            if (nums[i] < 0)
+                result *= -1;
+        }
+        return result;
+    }
+}
