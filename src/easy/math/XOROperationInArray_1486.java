57d6dd3db33a04e7f1a3b35646b22902d77a15de
Yevgeniy Batulin <yevgeniy.batulin@gmail.com>
2021-11-06 14:50:48 +0200

    Math

---
+++ src/easy/math/XOROperationInArray_1486.java
@@ -0,0 +1,11 @@
+package easy.math;
+
+public class XOROperationInArray_1486 {
+	public int xorOperation(int n, int start) {
+		int xor = 0;
+		for (int i = 0; i < n; i++) {
+			xor ^= start + 2 * i;
+		}
+		return xor;
+	}
+}
